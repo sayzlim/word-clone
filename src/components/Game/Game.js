c3677cc55e99d9b73ec9e4c27b0bb5c2c95f2dc9
Sayz Lim <github@sayzlim.net>
2023-02-19 10:05:15 +0700

    create GuessInput component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import GuessInput from '../GuessInput';
+
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
@@ -9,7 +11,9 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return <>
+  	<GuessInput/>
+  </>;
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function GuessInput() {
+	const [input, setInput] = React.useState('Guess');
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		const nextInput = input.toUpperCase();
+		console.log(`INPUT: ${nextInput}`);
+
+		if (nextInput.length < 5) {
+			alert('Please enter minimum 5 characters');
+		}
+
+		setInput('');
+	}
+
+	return (
+		<form
+			className="guess-input-wrapper"
+			onSubmit={(event) => handleSubmit(event)}
+		>
+			<label htmlFor="guess-input">Enter guess:</label>
+			<input
+				id="guess-input"
+				type="text"
+				maxLength="5"
+				value={input}
+				onChange={(event) => setInput(event.target.value)}
+			/>
+		</form>
+	);
+}
+
+export default GuessInput;
